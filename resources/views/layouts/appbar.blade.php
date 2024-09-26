713e41c41a6847c7ff86cf4300d8b11cb0f0f17f
RutananaArnold <arnoldrutanana@gmail.com>
2024-09-26 10:35:51 +0300

    nav bar profile pop up solved

--- resources/views/layouts/appbar.blade.php
+++ resources/views/layouts/appbar.blade.php
@@ -2,20 +2,20 @@
     <ul class="d-flex align-items-center">
 
         <li class="nav-item d-block d-lg-none">
-            <a class="nav-link nav-icon search-bar-toggle " href="#">
+            <a class="nav-link nav-icon search-bar-toggle" href="#">
                 <i class="bi bi-search"></i>
             </a>
-        </li><!-- End Search Icon-->
+        </li><!-- End Search Icon -->
 
         <li class="nav-item dropdown pe-3">
-
-            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <a class="nav-link nav-profile d-flex align-items-center pe-0 dropdown-toggle" href="#"
+                data-bs-toggle="dropdown">
                 <div
                     style="width: 40px; height: 40px; border-radius: 50%; overflow: hidden; background-color: #ccc; display: flex; justify-content: center; align-items: center;">
                     <i class="bi bi-person-fill" style="font-size: 48px; color: white;"></i>
                 </div>
-                <span class="d-none d-md-block dropdown-toggle ps-2">{{ auth()->user()->name }}</span>
-            </a><!-- End Profile Iamge Icon -->
+                <span class="d-none d-md-block ps-2">{{ auth()->user()->name }}</span>
+            </a><!-- End Profile Image Icon -->
 
             <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                 <li class="dropdown-header">
@@ -25,26 +25,28 @@
                 <li>
                     <hr class="dropdown-divider">
                 </li>
-
                 <li>
                     <a class="dropdown-item d-flex align-items-center" href="{{ route('profile-page') }}">
-                        <i class="bi bi-person"></i>
-                        <span>My Profile</span>
+                        <i class="bi bi-person"></i> <span>My Profile</span>
                     </a>
                 </li>
                 <li>
                     <hr class="dropdown-divider">
                 </li>
-
                 <li>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button class="bi bi-box-arrow-right" type="submit"><span>Sign Out</span></button>
+                        <button type="submit" class="dropdown-item d-flex align-items-center">
+                            <i class="bi bi-box-arrow-right"></i> <span>Sign Out</span>
+                        </button>
                     </form>
                 </li>
-
             </ul><!-- End Profile Dropdown Items -->
         </li><!-- End Profile Nav -->
 
     </ul>
 </nav><!-- End Icons Navigation -->
+
+<!-- Bootstrap JS and Popper.js (Place before closing body tag) -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -19,9 +19,6 @@
     {{-- Country code CSS --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css">
 
-    {{--  --}}
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
-
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link
